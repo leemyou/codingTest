127c0a4ee653a7be512cb89bb6e88979bf0ef58c
Hyohyeon Lim <limhyeon0809@gmail.com>
2022-04-28 01:53:12 +0900

    0428 home commit

--- prectice/test/test003.js
+++ prectice/test/test003.js
@@ -20,18 +20,27 @@ for (var i in lottos) {
 
 // arr배열의 선언 및 길이 지정
 let arr = new Array(count0).fill(0); //[0,0]
+expectArr = [];
 
 for(i in arr) {
     for(j=1; j<46; j++){
         // expectArr = 0대신 1~45로 구성된 새로운 배열생성 및 구성
-        // expextArr[0]과 expectArr[1]의 중복제거(원소들 간의 중복제거)
-        // lottos 안에 있는 숫자들 중에 하나랑 중복이 있나 검사해야함
+        var t = arr.splice(i, 1, j);
+        expectArr.push(t);
     }
 }
 
-만들어진 배열중에 하나를 고르고(for문) 0을뺀 나머지lottos와 합쳐
+
+
+        // expextArr[0]과 expectArr[1]의 중복제거(원소들 간의 중복제거)
+        // 중복제거Arr = [... new Set(expextArr)];
+        // lottos 안에 있는 숫자들 중에 하나랑 중복이 있나 검사해야함
+    // }
+// }
+
+// 만들어진 배열중에 하나를 고르고(for문) 0을뺀 나머지lottos와 합쳐
 
 
 
 
-console.log(expNum);
+console.log(expectArr);
