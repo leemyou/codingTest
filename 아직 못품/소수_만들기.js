8e3f0e22af8efc387663e615a7888fa165d569f6
Hyohyeon Lim <limhyeon0809@gmail.com>
2022-05-07 02:01:10 +0900

    third commit

---
+++ 아직 못품/소수_만들기.js
@@ -0,0 +1,16 @@
+function solution(nums) {
+    var answer = -1;
+    
+    nums.forEach((fixed, index, origin) => {
+        const rest = 
+    });
+
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    console.log('Hello Javascript')
+    
+    return answer;
+}
+
+result = solution([1,2,3,4]);
+console.log(result)
